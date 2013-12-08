3a5ca164a1f4ad2bfdfa3674222370a602da2e7f
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-12-08 16:02:21 -0200

    Revert "Revert "wip turbolinks""
    This reverts commit e55f5f7bd87d11fef4bc1fa0376c6fa02aad3cf6.

---
+++ _assets/up.js
@@ -0,0 +1,10 @@
+$(document).on('page:fetch', function () {
+  NProgress.start();
+});
+$(document).on('page:change', function () {
+  NProgress.done();
+});
+$(document).on('page:restore', function () {
+  NProgress.remove();
+});
+NProgress.configure({ showSpinner: false });
